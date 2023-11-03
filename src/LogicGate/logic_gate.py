3fdbe7d77bb482eed3280bccbf94845d40a475aa
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-02 20:51:54 -0400

    Ran linters

--- src/LogicGate/logic_gate.py
+++ src/LogicGate/logic_gate.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
+
 class LogicGateError(Exception):
     ...
 
-class LogicGate:
 
+class LogicGate:
     def __init__(self):
         self._name: str = ""
         self._output_pin: Optional[int] = None
@@ -12,23 +13,23 @@ class LogicGate:
     @property
     def name(self) -> str:
         return self._name
-    
+
     @name.setter
     def name(self, value: str) -> None:
         self._name = value
 
     def _logic(self) -> None:
         raise NotImplementedError("`_logic` needs to be implemented!")
-    
+
     def get_output_pin(self) -> int:
         self._logic()
         return self._output_pin
-    
+
     def set_input_pin(self, value: int, pin: int = 0) -> None:
         raise NotImplementedError("`set_input_pin` needs to be implemented!")
-    
-class BinaryGate(LogicGate):
 
+
+class BinaryGate(LogicGate):
     def __init__(self):
         super(self, BinaryGate).__init__()
         self._input0_pin: Optional[int] = None
@@ -48,8 +49,8 @@ class BinaryGate(LogicGate):
         else:
             raise LogicGateError(f"Entered an unknown pin: {pin}!")
 
-class UnaryGate(LogicGate):
 
+class UnaryGate(LogicGate):
     def __init__(self):
         super(self, UnaryGate).__init__()
         self._input0_pin: Optional[int] = None
