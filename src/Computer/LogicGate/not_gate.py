4591bf1f6070d104a75e6a82e5a15f728d355534
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-03 13:44:18 -0400

    comp1.5 - updated the docstrings for the three implemented classes

--- src/Computer/LogicGate/not_gate.py
+++ src/Computer/LogicGate/not_gate.py
@@ -3,10 +3,43 @@ from .unary_gate import UnaryGate
 
 
 class NotGate(UnaryGate):
+
+    """
+    This implements a not gate. This "is a" type of unary gate. This will implement the
+    logic of the Boolean `'and'` operation. It will inherit all of the attributes and
+    behaviors of the `UnaryGate` parent class.
+
+    Attributes:
+        input0_pin:     the first input pin (private)
+        name:           the name of the logic gate class (public)
+        output_pin:     the output of the pin that set later on by the logic (private)
+    """
+
     def __init__(self):
+        """Constructor..."""
+
         super().__init__()
+        """
+        This inherits from the `UnaryGate` parent class.
+        """
 
     def _logic(self) -> None:
+        """
+        This implements the logic of the Boolean `'not'` operation. It takes the value
+        of the one input pin and will set the resulting value to the output pin.
+
+            pin0 | output
+            -------------
+              0  |   1
+              1  |   0
+
+        This is a private method not intended to be directly called by the user.
+
+        Raises:
+            LogicGateError:
+                The first input pin has not been set!
+        """
+
         if self._input0_pin is None:
             raise LogicGateError("The first input pin has not been set!")
 
--- src/Computer/LogicGate/or_gate.py
+++ src/Computer/LogicGate/or_gate.py
@@ -3,10 +3,47 @@ from .logic_gate import LogicGateError
 
 
 class OrGate(BinaryGate):
+
+    """
+    This implements an or gate. This "is a" type of binary gate. This will implement
+    the logic of the Boolean `'or'` operation. It will inherit all of the attributes
+    and behaviors of the `BinaryGate` parent class.
+
+    Attributes:
+        input0_pin:     the first input pin (private)
+        input1_pin:     the second input pin (private)
+        name:           the name of the logic gate class (public)
+        output_pin:     the output of the pin that set later on by the logic (private)
+    """
+
     def __init__(self):
+        """Constructor..."""
+
         super().__init__()
+        """
+        This inherits from the `BinaryGate` parent class.
+        """
 
     def _logic(self) -> None:
+        """
+        This implements the logic of the Boolean `'or'` operation. It takes the value
+        of the two input pins and will set the resulting value to the output pin.
+
+            pin0 | pin1 | output
+            --------------------
+              0  |  0   |    0
+              0  |  1   |    1
+              1  |  0   |    1
+              1  |  1   |    1
+
+        This is a private method not intended to be directly called by the user.
+
+        Raises:
+            LogicGateError:
+                (1) The first input pin has not been set!
+                (2) The second input pin has not been set!
+        """
+
         if self._input0_pin is None:
             raise LogicGateError("The first input pin has not been set!")
         elif self._input1_pin is None:
