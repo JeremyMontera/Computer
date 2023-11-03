2b6db087954fdf4e13cd1dca416418bf427a7548
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-03 13:24:06 -0400

    Ran linters

--- src/Computer/LogicGate/or_gate.py
+++ src/Computer/LogicGate/or_gate.py
@@ -1,8 +1,8 @@
 from .binary_gate import BinaryGate
 from .logic_gate import LogicGateError
 
+
 class OrGate(BinaryGate):
-    
     def __init__(self):
         super().__init__()
 
@@ -11,12 +11,12 @@ class OrGate(BinaryGate):
             raise LogicGateError("The first input pin has not been set!")
         elif self._input1_pin is None:
             raise LogicGateError("The second input pin has not been set!")
-        
+
         output = bool(self._input0_pin) or bool(self._input1_pin)
 
         print(
-            f"[00:00:00] {bool(self._input0_pin)} or {bool(self._input1_pin)} is " \
+            f"[00:00:00] {bool(self._input0_pin)} or {bool(self._input1_pin)} is "
             f"{output}."
         )
-        
+
         return output
