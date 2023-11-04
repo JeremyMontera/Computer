424e617e01be6b5df9d826c095bc7c6aed6725a5
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-04 09:25:20 -0400

    comp2.1 - continued to fill in Connection methods

--- src/Computer/Connection/connection.py
+++ src/Computer/Connection/connection.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from Computer.LogicGate import LogicGate
 
+class ConnectionError(Exception):
+    ...
+
 class Connection:
 
     def __init__(self):
@@ -9,10 +12,18 @@ class Connection:
         self._output: Optional['LogicGate'] = None
 
     def feed(self) -> int:
-        ...
+        if self._output is None:
+            raise ConnectionError("The output hasn't been connected yet!")
+        
+        print(f"[00:00:00] Feeding input gate {self._input.name}'s output.")
+        return self._input.get_output_pin()
 
     def set_input(self, gate: 'LogicGate') -> None:
-        ...
+        if gate.has_output_set():
+            raise ConnectionError(f"{gate.name}'s output pin is already set!")
+        
+        print(f"[00:00:00] Setting {gate.name}'s output pin to the input.")
+        self._input = gate
 
     def set_output(self, gate: 'LogicGate') -> None:
         ...
