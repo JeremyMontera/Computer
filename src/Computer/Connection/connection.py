239c50bf273c64d3a17277415f1ee23d5f00f023
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-04 09:14:25 -0400

    comp2.2 - started filling in the requisite methods for Connection class

--- src/Computer/Connection/connection.py
+++ src/Computer/Connection/connection.py
@@ -1,5 +1,18 @@
+from typing import Optional
+
 from Computer.LogicGate import LogicGate
 
 class Connection:
 
-    ...
+    def __init__(self):
+        self._input: Optional['LogicGate'] = None
+        self._output: Optional['LogicGate'] = None
+
+    def feed(self) -> int:
+        ...
+
+    def set_input(self, gate: 'LogicGate') -> None:
+        ...
+
+    def set_output(self, gate: 'LogicGate') -> None:
+        ...
