522d78e4d7128f223ac2e324e2b57dc320ad6ed9
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-09 16:58:11 -0500

    Ran linters

--- src/Computer/Bit/bit.py
+++ src/Computer/Bit/bit.py
@@ -3,15 +3,17 @@ from typing import Union
 
 BitValueType = Union[int, bool, str]
 
+
 class BitValue(enum.Enum):
     OFF: int = 0
     ON: int = 1
 
+
 class BitError(Exception):
     ...
 
-class Bit:
 
+class Bit:
     def __init__(self, value: BitValueType):
         self._value: BitValue = self._convert_to_bit_value(value)
 
@@ -39,4 +41,4 @@ class Bit:
             else:
                 raise BitError(f"Entered an unknown value: {value}!")
         except ValueError:
-            raise BitError(f"Entered something that cannot be handled!")
+            raise BitError("Entered something that cannot be handled!")
