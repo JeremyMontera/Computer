362ed693a8866544fcc4de972b39473849672fa8
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-17 00:07:33 -0500

    comp5 - commented out compound gates; will address in next task

--- test/LogicCircuit/test_nand_gate.py
+++ test/LogicCircuit/test_nand_gate.py
@@ -1,59 +1,59 @@
-from unittest import mock
+# from unittest import mock
 
-import pytest
+# import pytest
 
-from Computer.LogicCircuit import AndGate, NandGate, NotGate
-from Computer.LogicCircuit.LogicGate.logic_gate import LogicGateError
-
-
-@pytest.fixture
-def nand_gate():
-    return NandGate()
+# from Computer.LogicCircuit import AndGate, NandGate, NotGate
+# from Computer.LogicCircuit.LogicGate.logic_gate import LogicGateError
 
 
-def test_nand_gate_init(nand_gate):
-    assert hasattr(nand_gate, "_input0_pin")
-    assert nand_gate._input0_pin is None
-    assert hasattr(nand_gate, "_input1_pin")
-    assert nand_gate._input1_pin is None
-    assert hasattr(nand_gate, "_output_pin")
-    assert nand_gate._output_pin is None
-    assert hasattr(nand_gate, "_gate0")
-    assert isinstance(nand_gate._gate0, AndGate)
-    assert nand_gate._gate0.name == "nand :: and gate"
-    assert hasattr(nand_gate, "_gate1")
-    assert isinstance(nand_gate._gate1, NotGate)
-    assert nand_gate._gate1.name == "nand :: not gate"
-    assert hasattr(nand_gate, "_conn0")
+# @pytest.fixture
+# def nand_gate():
+#     return NandGate()
 
 
-@mock.patch.object(NotGate, "get_output_pin")
-def test_nand_gate_get_output_pin(mock_out, nand_gate):
-    nand_gate.get_output_pin()
-    mock_out.assert_called_once
+# def test_nand_gate_init(nand_gate):
+#     assert hasattr(nand_gate, "_input0_pin")
+#     assert nand_gate._input0_pin is None
+#     assert hasattr(nand_gate, "_input1_pin")
+#     assert nand_gate._input1_pin is None
+#     assert hasattr(nand_gate, "_output_pin")
+#     assert nand_gate._output_pin is None
+#     assert hasattr(nand_gate, "_gate0")
+#     assert isinstance(nand_gate._gate0, AndGate)
+#     assert nand_gate._gate0.name == "nand :: and gate"
+#     assert hasattr(nand_gate, "_gate1")
+#     assert isinstance(nand_gate._gate1, NotGate)
+#     assert nand_gate._gate1.name == "nand :: not gate"
+#     assert hasattr(nand_gate, "_conn0")
 
 
-def test_nand_gate_has_input_pin_set(nand_gate):
-    nand_gate._input0_pin = 0
-    nand_gate._input1_pin = 1
-    assert nand_gate.has_input_pin_set(pin=0)
-    assert nand_gate.has_input_pin_set(pin=1)
+# @mock.patch.object(NotGate, "get_output_pin")
+# def test_nand_gate_get_output_pin(mock_out, nand_gate):
+#     nand_gate.get_output_pin()
+#     mock_out.assert_called_once
 
 
-def test_nand_gate_has_input_pin_set_error(nand_gate):
-    with pytest.raises(LogicGateError) as exc:
-        nand_gate.has_input_pin_set(pin=2)
-
-    assert exc.value.args[0] == "Entered an unknown pin: 2!"
-
-
-def test_nand_gate_has_output_pin_set(nand_gate):
-    assert not nand_gate.has_output_pin_set()
-    nand_gate._output_pin = 0
-    assert nand_gate.has_output_pin_set()
+# def test_nand_gate_has_input_pin_set(nand_gate):
+#     nand_gate._input0_pin = 0
+#     nand_gate._input1_pin = 1
+#     assert nand_gate.has_input_pin_set(pin=0)
+#     assert nand_gate.has_input_pin_set(pin=1)
 
 
-@mock.patch.object(AndGate, "set_input_pin")
-def test_nand_gate_set_input_pin(mock_in, nand_gate):
-    nand_gate.set_input_pin(0, pin=1)
-    mock_in.assert_called_once_with(0, pin=1)
+# def test_nand_gate_has_input_pin_set_error(nand_gate):
+#     with pytest.raises(LogicGateError) as exc:
+#         nand_gate.has_input_pin_set(pin=2)
+
+#     assert exc.value.args[0] == "Entered an unknown pin: 2!"
+
+
+# def test_nand_gate_has_output_pin_set(nand_gate):
+#     assert not nand_gate.has_output_pin_set()
+#     nand_gate._output_pin = 0
+#     assert nand_gate.has_output_pin_set()
+
+
+# @mock.patch.object(AndGate, "set_input_pin")
+# def test_nand_gate_set_input_pin(mock_in, nand_gate):
+#     nand_gate.set_input_pin(0, pin=1)
+#     mock_in.assert_called_once_with(0, pin=1)
--- test/LogicCircuit/test_nor_gate.py
+++ test/LogicCircuit/test_nor_gate.py
@@ -1,59 +1,59 @@
-from unittest import mock
+# from unittest import mock
 
-import pytest
+# import pytest
 
-from Computer.LogicCircuit import NorGate, NotGate, OrGate
-from Computer.LogicCircuit.LogicGate.logic_gate import LogicGateError
-
-
-@pytest.fixture
-def nor_gate():
-    return NorGate()
+# from Computer.LogicCircuit import NorGate, NotGate, OrGate
+# from Computer.LogicCircuit.LogicGate.logic_gate import LogicGateError
 
 
-def test_nor_gate_init(nor_gate):
-    assert hasattr(nor_gate, "_input0_pin")
-    assert nor_gate._input0_pin is None
-    assert hasattr(nor_gate, "_input1_pin")
-    assert nor_gate._input1_pin is None
-    assert hasattr(nor_gate, "_output_pin")
-    assert nor_gate._output_pin is None
-    assert hasattr(nor_gate, "_gate0")
-    assert isinstance(nor_gate._gate0, OrGate)
-    assert nor_gate._gate0.name == "nor :: or gate"
-    assert hasattr(nor_gate, "_gate1")
-    assert isinstance(nor_gate._gate1, NotGate)
-    assert nor_gate._gate1.name == "nor :: not gate"
-    assert hasattr(nor_gate, "_conn0")
+# @pytest.fixture
+# def nor_gate():
+#     return NorGate()
 
 
-@mock.patch.object(NotGate, "get_output_pin")
-def test_nor_gate_get_output_pin(mock_out, nor_gate):
-    nor_gate.get_output_pin()
-    mock_out.assert_called_once
+# def test_nor_gate_init(nor_gate):
+#     assert hasattr(nor_gate, "_input0_pin")
+#     assert nor_gate._input0_pin is None
+#     assert hasattr(nor_gate, "_input1_pin")
+#     assert nor_gate._input1_pin is None
+#     assert hasattr(nor_gate, "_output_pin")
+#     assert nor_gate._output_pin is None
+#     assert hasattr(nor_gate, "_gate0")
+#     assert isinstance(nor_gate._gate0, OrGate)
+#     assert nor_gate._gate0.name == "nor :: or gate"
+#     assert hasattr(nor_gate, "_gate1")
+#     assert isinstance(nor_gate._gate1, NotGate)
+#     assert nor_gate._gate1.name == "nor :: not gate"
+#     assert hasattr(nor_gate, "_conn0")
 
 
-def test_nor_gate_has_input_pin_set(nor_gate):
-    nor_gate._input0_pin = 0
-    nor_gate._input1_pin = 1
-    assert nor_gate.has_input_pin_set(pin=0)
-    assert nor_gate.has_input_pin_set(pin=1)
+# @mock.patch.object(NotGate, "get_output_pin")
+# def test_nor_gate_get_output_pin(mock_out, nor_gate):
+#     nor_gate.get_output_pin()
+#     mock_out.assert_called_once
 
 
-def test_nor_gate_has_input_pin_set_error(nor_gate):
-    with pytest.raises(LogicGateError) as exc:
-        nor_gate.has_input_pin_set(pin=2)
-
-    assert exc.value.args[0] == "Entered an unknown pin: 2!"
-
-
-def test_nor_gate_has_output_pin_set(nor_gate):
-    assert not nor_gate.has_output_pin_set()
-    nor_gate._output_pin = 0
-    assert nor_gate.has_output_pin_set()
+# def test_nor_gate_has_input_pin_set(nor_gate):
+#     nor_gate._input0_pin = 0
+#     nor_gate._input1_pin = 1
+#     assert nor_gate.has_input_pin_set(pin=0)
+#     assert nor_gate.has_input_pin_set(pin=1)
 
 
-@mock.patch.object(OrGate, "set_input_pin")
-def test_nor_gate_set_input_pin(mock_in, nor_gate):
-    nor_gate.set_input_pin(0, pin=1)
-    mock_in.assert_called_once_with(0, pin=1)
+# def test_nor_gate_has_input_pin_set_error(nor_gate):
+#     with pytest.raises(LogicGateError) as exc:
+#         nor_gate.has_input_pin_set(pin=2)
+
+#     assert exc.value.args[0] == "Entered an unknown pin: 2!"
+
+
+# def test_nor_gate_has_output_pin_set(nor_gate):
+#     assert not nor_gate.has_output_pin_set()
+#     nor_gate._output_pin = 0
+#     assert nor_gate.has_output_pin_set()
+
+
+# @mock.patch.object(OrGate, "set_input_pin")
+# def test_nor_gate_set_input_pin(mock_in, nor_gate):
+#     nor_gate.set_input_pin(0, pin=1)
+#     mock_in.assert_called_once_with(0, pin=1)
