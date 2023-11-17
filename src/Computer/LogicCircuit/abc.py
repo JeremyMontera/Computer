61ac9fc56af2389d0ab3a10f35debb4758d65bf8
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-11-16 22:54:10 -0500

    comp5.4.3 - updated IConnection interface; made methods abstractbasemethods

--- src/Computer/LogicCircuit/abc.py
+++ src/Computer/LogicCircuit/abc.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import abc
 import enum
 from typing import Optional, TypeVar
@@ -6,21 +8,54 @@ E = TypeVar("E", bound=enum.Enum)
 
 
 class IConnection(metaclass=abc.ABCMeta):
-    ...
 
-
-class ILogicGate(metaclass=abc.ABCMeta):
-    def __init__(self, type: Optional[E] = None, name: Optional[str] = None):
+    @abc.abstractclassmethod
+    def __init__(self):
         ...
 
-    def get_output_pin(self) -> int:
+    @abc.abstractclassmethod
+    def feed(self) -> int:
         ...
 
-    def has_input_pin_set(self, pin: int = 0) -> bool:
+    @abc.abstractclassmethod
+    def has_input_connection_set(self) -> bool:
         ...
 
+    @abc.abstractclassmethod
+    def has_output_connection_set(self) -> bool:
+        ...
+
+    @abc.abstractclassmethod
     def reset(self) -> None:
         ...
 
+    @abc.abstractclassmethod
+    def set_input_connection(self, gate: ILogicGate) -> None:
+        ...
+
+    @abc.abstractclassmethod
+    def set_output_connection(self, gate: ILogicGate) -> None:
+        ...
+
+
+class ILogicGate(metaclass=abc.ABCMeta):
+
+    @abc.abstractclassmethod
+    def __init__(self, type: Optional[E] = None, name: Optional[str] = None):
+        ...
+
+    @abc.abstractclassmethod
+    def get_output_pin(self) -> int:
+        ...
+
+    @abc.abstractclassmethod
+    def has_input_pin_set(self, pin: int = 0) -> bool:
+        ...
+
+    @abc.abstractclassmethod
+    def reset(self) -> None:
+        ...
+
+    @abc.abstractclassmethod
     def set_input_pin(self, value: int | IConnection = 0, pin: int = 0) -> None:
         ...
